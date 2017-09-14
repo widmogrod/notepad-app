46dfdc9eff9159979aaf8f96ef86e87f112c8b63
widmogrod <widmogrod@gmail.com>
2017-09-14 22:30:14 +0200

    quilljs

--- build/main.js
+++ build/main.js
@@ -1,80 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const js_crdt_1 = require("js-crdt");
-const Rx_1 = require("rxjs/Rx");
+const text_1 = require("js-crdt/build/text");
+require("rxjs/add/operator/map");
 const queueing_subject_1 = require("queueing-subject");
 const rxjs_websockets_1 = require("rxjs-websockets");
 const serialiser_1 = require("./serialiser");
-function bench(name, func, ctx) {
-    return function () {
-        const start = new Date();
-        const result = func.apply(ctx, arguments);
-        const end = new Date();
-        // console.log({name, time: end.getTime() - start.getTime()});
-        return result;
-    };
-}
 function uuid() {
     const array = new Uint8Array(2);
     crypto.getRandomValues(array);
     return array.join('-');
 }
-function shiftCursorPositionRelativeTo(text, position, diff) {
-    diff = diff | 0;
-    return text.reduce(({ shiftBy, position }, operations) => {
-        return operations.reduce(({ shiftBy, position }, operation) => {
-            if (operation instanceof js_crdt_1.default.text.Insert) {
-                if (operation.at <= (position + diff)) {
-                    shiftBy += operation.value.length;
-                    position += operation.value.length;
-                }
-            }
-            else if (operation instanceof js_crdt_1.default.text.Delete) {
-                if (operation.at < position) {
-                    shiftBy -= operation.length;
-                }
-            }
-            return { shiftBy, position };
-        }, { shiftBy, position });
-    }, { shiftBy: 0, position }).shiftBy;
-}
-let editorElement = document.getElementById('editor');
-// Observable.fromEvent(editorElement, 'select').subscribe(e => console.log(e))
-// Observable.fromEvent(editorElement, 'click').subscribe(e => console.log(e))
-// Observable.fromEvent(editorElement, 'focus').subscribe(e => console.log(e))
-// Stream of requestAnimationFrame ticks
-const animationFrame = Rx_1.Observable.interval(0, Rx_1.Scheduler.animationFrame);
-var keyDowns = Rx_1.Observable
-    .fromEvent(document, 'keydown')
-    .map((e) => {
-    e.preventDefault();
-    return e;
-});
-var keyUps = Rx_1.Observable
-    .fromEvent(document, 'keyup')
-    .map((e) => {
-    e.preventDefault();
-    return e;
-});
-var keyPresses = keyDowns
-    .merge(keyUps)
-    .groupBy((e) => e.keyCode)
-    .map(group => {
-    return group.distinctUntilChanged((x, y) => x.type !== y.type);
-})
-    .mergeAll();
-keyPresses.subscribe(e => console.log(e));
-let positionSelection = animationFrame
-    .map(() => p2s(({ target: editorElement })))
-    .distinctUntilChanged((x, y) => x.pos === y.pos && x.selection === y.selection);
-positionSelection.subscribe(e => console.log(e));
-let charInputsStream = keyPresses
-    .withLatestFrom(positionSelection, (e, p) => {
-    return { e: e, pos: p.pos, selection: p.selection };
-});
-// charInputsStream.subscribe(e => console.log(e))
-let cutStream = Rx_1.Observable.fromEvent(editorElement, 'cut');
-let pasteStream = Rx_1.Observable.fromEvent(editorElement, 'paste');
 let host = window.document.location.host.replace(/:.*/, '');
 let port = window.document.location.port;
 let protocol = window.document.location.protocol.match(/s:$/) ? 'wss' : 'ws';
@@ -85,125 +21,59 @@ const publish = new queueing_subject_1.QueueingSubject();
 // this method returns an object which contains two observables
 const { messages, connectionStatus } = rxjs_websockets_1.default(WebSocketURL, publish);
 connectionStatus.subscribe(e => console.log({ status: e }));
-const BACKSPACE = 8;
-const DELETE = 46;
-const ENTER = 13;
-function p2s(e) {
-    const pos = e.target.selectionStart;
-    const selection = e.target.selectionEnd - pos;
-    return { pos, selection };
-}
-const chars = charInputsStream
-    .filter(({ e }) => {
-    return e.key.length === 1;
-})
-    .map(({ e, pos, selection }) => {
-    return selection
-        ? Rx_1.Observable.from([
-            new js_crdt_1.default.text.Delete(pos, selection),
-            new js_crdt_1.default.text.Insert(pos, e.key),
-        ])
-        : Rx_1.Observable.from([
-            new js_crdt_1.default.text.Insert(pos, e.key),
-        ]);
-})
-    .concatAll();
-const backspaceKey = charInputsStream.filter(({ e }) => e.keyCode === BACKSPACE).map(({ pos, selection }) => {
-    return selection
-        ? new js_crdt_1.default.text.Delete(pos, selection)
-        : new js_crdt_1.default.text.Delete(pos - 1, 1);
+// This is hack to properly require quill :/
+const Quill = require("quill");
+var editor = new Quill('#editor', {
+    modules: {
+        toolbar: false,
+    },
+    theme: 'snow'
 });
-const deleteKey = charInputsStream.filter(({ e }) => e.keyCode === DELETE).map(({ pos, selection }) => {
-    return selection
-        ? new js_crdt_1.default.text.Delete(pos, selection)
-        : new js_crdt_1.default.text.Delete(pos, 1);
-});
-const enterKey = charInputsStream.filter(({ e }) => e.keyCode === ENTER).map(({ pos, selection }) => {
-    return selection
-        ? Rx_1.Observable.from([
-            new js_crdt_1.default.text.Delete(pos, selection),
-            new js_crdt_1.default.text.Insert(pos, "\n"),
-        ])
-        : Rx_1.Observable.from([
-            new js_crdt_1.default.text.Insert(pos, "\n"),
-        ]);
-}).concatAll();
-const cutOp = cutStream.map((e) => {
-    const { pos, selection } = p2s(e);
-    return selection
-        ? Rx_1.Observable.of(new js_crdt_1.default.text.Delete(pos, selection))
-        : Rx_1.Observable.never();
-}).concatAll();
-const pasteOp = pasteStream.map((e) => {
-    const { pos, selection } = p2s(e);
-    return selection
-        ? Rx_1.Observable.from([
-            new js_crdt_1.default.text.Delete(pos, selection),
-            new js_crdt_1.default.text.Insert(pos, e.clipboardData.getData('text/plain'))
-        ])
-        : Rx_1.Observable.from([
-            new js_crdt_1.default.text.Insert(pos, e.clipboardData.getData('text/plain'))
-        ]);
-}).concatAll();
-chars
-    .merge(backspaceKey)
-    .merge(deleteKey)
-    .merge(enterKey)
-    .merge(cutOp)
-    .merge(pasteOp)
-    .subscribe((op) => {
-    console.log(op);
-    database = database.next();
-    database.apply(op);
-    const s = op instanceof js_crdt_1.default.text.Insert
-        ? (op.at + op.value.length)
-        : (op instanceof js_crdt_1.default.text.Delete
-            ? (op.at)
-            : 0);
-    editorElement.value = renderer(database);
-    console.log({ selection: s });
-    editorElement.setSelectionRange(s, s);
-    const data = serialiser_1.serialise(database);
-    publish.next(data);
-});
-messages
-    .map(bench('ws-deserialise', serialiser_1.deserialise))
-    .bufferTime(100)
-    .subscribe(es => {
-    es.map((e) => {
+editor.on('text-change', function (delta, oldDelta, source) {
+    if (source !== "user") {
+        return;
+    }
+    const r = delta.ops.reduce((r, o) => {
+        if (o.retain) {
+            r.pos = o.retain;
+        }
+        else if (o.insert) {
+            r.op = new js_crdt_1.default.text.Insert(r.pos, o.insert);
+        }
+        else if (o.delete) {
+            r.op = new js_crdt_1.default.text.Delete(r.pos, o.delete);
+        }
+        return r;
+    }, { pos: 0, op: null });
+    if (r.op) {
         database = database.next();
-        database = database.merge(e);
-        onFrame(render, setCursorOnUpdate)(e);
-    });
+        database.apply(r.op);
+        const data = serialiser_1.serialise(database);
+        publish.next(data);
+    }
 });
-function renderer(text) {
-    return js_crdt_1.default.text.renderString(text);
-}
-function onFrame(f1, f2) {
-    return (arg) => {
-        const start = editorElement.selectionStart;
-        const end = editorElement.selectionEnd;
-        const shiftBy = f2(arg, start, end);
-        // editorElement.setSelectionRange(start, end);
-        requestAnimationFrame(() => {
-            f1(arg);
-            editorElement.setSelectionRange(start + shiftBy, end + shiftBy);
-        });
-    };
-}
-function setCursorOnKey(e, start, end) {
-    return bench('cursor-key-calculate', () => {
-        return shiftCursorPositionRelativeTo(e, start);
-    })();
-}
-function setCursorOnUpdate(e, start, end) {
-    return bench('cursor-up-calculate', () => {
-        return shiftCursorPositionRelativeTo(e, start, -1);
-    })();
-}
-function render() {
-    const string = bench('render-string', () => {
-        return renderer(database);
-    })();
-    bench('render-set', () => editorElement.value = string)();
-}
+const QuillDelta = require("quill-delta");
+messages
+    .map(serialiser_1.deserialise)
+    .subscribe(e => {
+    const d = e.reduce((r, o) => {
+        return o.reduce((r, o) => {
+            if (o instanceof text_1.Insert) {
+                // return [
+                //   {retrain: o.at},
+                //   {insert: o.value}
+                // ];
+                return r.retain(o.at).insert(o.value);
+            }
+            else if (o instanceof text_1.Delete) {
+                return r.retain(o.at).insert(o.length);
+            }
+            return r;
+        }, r);
+    }, new QuillDelta());
+    database = database.next();
+    database = database.merge(e);
+    console.log(d);
+    const xx = editor.updateContents(d);
+    console.log(xx);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,114 +1,16 @@
 import crdt from 'js-crdt';
+import {Insert, Delete} from 'js-crdt/build/text';
 import {Observable, Scheduler} from 'rxjs/Rx';
+import "rxjs/add/operator/map";
 import { QueueingSubject } from 'queueing-subject';
 import websocketConnect from 'rxjs-websockets';
 import {serialise, deserialise} from './serialiser';
-
-function bench(name, func, ctx?) {
-  return function() {
-    const start = new Date();
-    const result = func.apply(ctx, arguments);
-    const end = new Date();
-
-    // console.log({name, time: end.getTime() - start.getTime()});
-
-    return result;
-  }
-}
 
 function uuid() {
   const array = new Uint8Array(2);
   crypto.getRandomValues(array);
   return array.join('-')
 }
-
-function shiftCursorPositionRelativeTo(text, position, diff?) {
-  diff = diff |0;
-  return text.reduce(({shiftBy, position}, operations) => {
-    return operations.reduce(({shiftBy, position}, operation) => {
-      if (operation instanceof crdt.text.Insert) {
-        if (operation.at <= (position + diff)) {
-          shiftBy += operation.value.length;
-          position += operation.value.length;
-        }
-      } else if (operation instanceof crdt.text.Delete) {
-        if (operation.at < position) {
-          shiftBy -= operation.length;
-        }
-      }
-
-      return {shiftBy, position};
-    }, {shiftBy, position});
-  }, {shiftBy: 0, position}).shiftBy;
-}
-
-interface Ev extends HTMLInputElement {
-  preventDefault()
-  type: string
-  code: number
-  key: string
-  keyCode: number
-  clipboardData: Cd
-  target: T
-}
-
-interface G {
-  target: T
-}
-
-interface T {
-  selectionStart: number
-  selectionEnd: number
-}
-interface Cd {
-  getData(string):string
-}
-
-let editorElement = <HTMLInputElement>document.getElementById('editor');
-// Observable.fromEvent(editorElement, 'select').subscribe(e => console.log(e))
-// Observable.fromEvent(editorElement, 'click').subscribe(e => console.log(e))
-// Observable.fromEvent(editorElement, 'focus').subscribe(e => console.log(e))
-// Stream of requestAnimationFrame ticks
-const animationFrame = Observable.interval(0, Scheduler.animationFrame);
-
-var keyDowns = Observable
-  .fromEvent(document, 'keydown')
-  .map((e: KeyboardEvent) => {
-    e.preventDefault();
-    return e;
-  });
-
-var keyUps = Observable
-  .fromEvent(document, 'keyup')
-  .map((e: KeyboardEvent) => {
-    e.preventDefault();
-    return e;
-  });
-
-var keyPresses = keyDowns
-  .merge(keyUps)
-  .groupBy((e: KeyboardEvent) => e.keyCode)
-  .map(group => {
-    return group.distinctUntilChanged((x: KeyboardEvent, y: KeyboardEvent) => x.type !== y.type)
-  })
-  .mergeAll()
-
-keyPresses.subscribe(e => console.log(e))
-let positionSelection = animationFrame
-  .map(() => p2s(<G>({target: editorElement})))
-  .distinctUntilChanged((x, y) => x.pos === y.pos && x.selection === y.selection);
-
-positionSelection.subscribe(e => console.log(e))
-
-let charInputsStream = keyPresses
-  .withLatestFrom(positionSelection, (e: KeyboardEvent, p: PosSelect) => {
-    return {e: e, pos: p.pos, selection: p.selection};
-  });
-
-// charInputsStream.subscribe(e => console.log(e))
-
-let cutStream = Observable.fromEvent(editorElement, 'cut');
-let pasteStream = Observable.fromEvent(editorElement, 'paste');
 
 let host = window.document.location.host.replace(/:.*/, '');
 let port = window.document.location.port;
@@ -126,148 +28,75 @@ const { messages, connectionStatus } = websocketConnect(WebSocketURL, publish)
 
 connectionStatus.subscribe(e => console.log({status: e}));
 
-const BACKSPACE = 8;
-const DELETE = 46;
-const ENTER = 13;
+// This is hack to properly require quill :/
+import * as Quill from 'quill';
 
-type PosSelect = {pos: number, selection: number}
-type KData = {e: KeyboardEvent, pos: number, selection: number}
+type QRetain = {retain: number}
+type QInsert = {insert: string}
+type QDelete = {delete: number}
+type QOperation = QRetain | QInsert | QDelete;
+type QSource = "user" | "api"
 
-function p2s(e: G): PosSelect {
-  const pos = e.target.selectionStart;
-  const selection = e.target.selectionEnd - pos;
-
-  return {pos, selection}
+interface Delta {
+  ops: QOperation[]
 }
 
-const chars = charInputsStream
-  .filter(({e}: KData) => {
-    return e.key.length === 1;
-  })
-  .map(({e, pos, selection}: KData) => {
-    return selection
-      ? Observable.from([
-        new crdt.text.Delete(pos, selection),
-        new crdt.text.Insert(pos, e.key),
-      ])
-      : Observable.from([
-        new crdt.text.Insert(pos, e.key),
-      ]);
-  })
-  .concatAll();
-
-const backspaceKey = charInputsStream.filter(({e}: KData) => e.keyCode === BACKSPACE).map(({pos, selection}:KData) => {
-  return selection
-    ? new crdt.text.Delete(pos, selection)
-    : new crdt.text.Delete(pos-1, 1);
+var editor = new Quill('#editor', {
+  modules: {
+    toolbar: false,
+  },
+  theme: 'snow'
 });
 
-const deleteKey = charInputsStream.filter(({e}: KData) => e.keyCode === DELETE).map(({pos, selection}: KData) => {
-  return selection
-    ? new crdt.text.Delete(pos, selection)
-    : new crdt.text.Delete(pos, 1);
-});
 
-const enterKey = charInputsStream.filter(({e}: KData) => e.keyCode === ENTER).map(({pos, selection}: KData) => {
-  return selection
-    ? Observable.from([
-      new crdt.text.Delete(pos, selection),
-      new crdt.text.Insert(pos, "\n"),
-    ])
-    : Observable.from([
-      new crdt.text.Insert(pos, "\n"),
-    ]);
-}).concatAll();
+type Operation = Insert | Delete;
+type OperationReducer = {pos: number, op: Operation | null};
 
-const cutOp = cutStream.map((e: Ev) => {
-  const {pos, selection} = p2s(e);
-  return selection
-    ? Observable.of(new crdt.text.Delete(pos, selection))
-    : Observable.never()
-}).concatAll();
+editor.on('text-change', function(delta: Delta, oldDelta: Delta, source: QSource) {
+  if (source !== "user") {
+    return;
+  }
 
-const pasteOp = pasteStream.map((e: Ev) => {
-  const {pos, selection} = p2s(e)
-  return selection
-    ? Observable.from([
-      new crdt.text.Delete(pos, selection),
-      new crdt.text.Insert(pos, e.clipboardData.getData('text/plain'))
-    ])
-    : Observable.from([
-      new crdt.text.Insert(pos, e.clipboardData.getData('text/plain'))
-    ])
-}).concatAll();
+  const r = delta.ops.reduce((r: OperationReducer, o: QOperation) => {
+    if ((<QRetain>o).retain) {
+      r.pos = (<QRetain>o).retain;
+    } else if ((<QInsert>o).insert) {
+      r.op = new crdt.text.Insert(r.pos, (<QInsert>o).insert);
+    } else if ((<QDelete>o).delete) {
+      r.op = new crdt.text.Delete(r.pos, (<QDelete>o).delete);
+    }
 
-chars
-  .merge(backspaceKey)
-  .merge(deleteKey)
-  .merge(enterKey)
-  .merge(cutOp)
-  .merge(pasteOp)
-  .subscribe((op) => {
+    return r;
+  }, {pos: 0, op: null});
+
+  if (r.op) {
     database = database.next();
-    database.apply(op);
-
-    const s = op instanceof crdt.text.Insert
-      ? (op.at + op.value.length)
-      : (op instanceof crdt.text.Delete
-        ? (op.at)
-        : 0);
-
-    editorElement.value = renderer(database);
-    editorElement.setSelectionRange(s,s);
+    database.apply(r.op);
 
     const data = serialise(database);
     publish.next(data);
-  })
-;
+  }
+});
+
+import * as QuillDelta from 'quill-delta'
 
 messages
-  .map(bench('ws-deserialise', deserialise))
-  .bufferTime(100)
-  .subscribe(es => {
-    es.map((e) => {
-      database = database.next();
-      database = database.merge(e);
-      onFrame(render, setCursorOnUpdate)(e);
-    });
-  })
-;
+  .map(deserialise)
+  .subscribe(e => {
+    const d = e.reduce((r: QuillDelta, o: Operation[]) => {
+      return o.reduce((r: QuillDelta, o: Operation) => {
+        if (o instanceof Insert) {
+          return r.retain(o.at).insert(o.value)
+        } else if (o instanceof Delete) {
+          return r.retain(o.at).insert(o.length)
+        }
+        return r;
 
-function renderer(text) {
-  return crdt.text.renderString(text);
-}
+      }, r);
+    }, new QuillDelta());
 
-function onFrame(f1, f2) {
-  return (arg) => {
-    const start = editorElement.selectionStart;
-    const end = editorElement.selectionEnd;
-    const shiftBy = f2(arg, start, end);
-    // editorElement.setSelectionRange(start, end);
-    requestAnimationFrame(() => {
-      f1(arg);
-      editorElement.setSelectionRange(start + shiftBy, end + shiftBy);
-    });
-  };
-}
+    database = database.next();
+    database = database.merge(e);
 
-function setCursorOnKey(e, start, end) {
-  return bench('cursor-key-calculate', () => {
-    return shiftCursorPositionRelativeTo(e, start);
-  })();
-}
-
-function setCursorOnUpdate(e, start, end) {
-  return bench('cursor-up-calculate', () => {
-    return shiftCursorPositionRelativeTo(e, start, -1);
-  })();
-}
-
-function render() {
-  const string = bench('render-string', () => {
-    return renderer(database);
-  })();
-
-  bench('render-set', () => editorElement.value = string)();
-}
-
+    editor.updateContents(d);
+  });
